bd4173313dd4829c9e094bc9e1f5096fafb28af9
Chidoka Ifeanyi <114946207+Emm-aan1@users.noreply.github.com>
2023-05-14 17:35:23 +0100

    Update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -10,50 +10,63 @@ function Experience() {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
+    
             <article className="experience__details">
               <h4>HTML</h4>
               <small className='text-light'>Experienced</small>
             </article>
+    
             <article className="experience__details">
               <h4>CSS</h4>
               <small className='text-light'>Experience</small>
             </article>
+    
             <article className="experience__details">
               <h4>JAVASCRIPT</h4>
               <small className='text-light'>Intermediate</small>
             </article>
+    
             <article className="experience__details">
               <h4>BOOTSTRAP</h4>
               <small className='text-light'>Experienced</small>
             </article>
+    
             <article className="experience__details">
               <h4>REACT</h4>
               <small className='text-light'>Intermediate</small>
             </article>
+    
             <article className="experience__details">
               <h4>TAILWIND</h4>
               <small className='text-light'>Intermediate</small>
             </article>
+    
             <article className="experience__details">
               <h4>TYPESCRIPT</h4>
               <small className='text-light'>Intermediate</small>
             </article>
+    
           </div>
         </div>
 
+    
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
+    
             <article className="experience__details">
               <h4>PHP</h4>
               <small className='text-light'>Intermediate</small>
             </article>
+    
             <article className="experience__details">
               <h4>MySQL</h4>
               <small className='text-light'>Advance Beginner</small>
             </article>
+    
           </div>
         </div>
+    
       </div>
     </section>
   )
