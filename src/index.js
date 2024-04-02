88b0f31c1725e4011e2dc0a497aaee25b1a30e9e
Chidoka Ifeanyi <114946207+Emm-aan1@users.noreply.github.com>
2024-04-02 23:51:24 +0100

    change 'root' to 'roots' in index.html and index.js files

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from "react-dom";
 import App from './App'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('roots'));
 root.render(<App />)
